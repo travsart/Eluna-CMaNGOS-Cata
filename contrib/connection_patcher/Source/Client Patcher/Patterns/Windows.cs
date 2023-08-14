729fa6b5b26bda5b8ff4af796416f7c9ce0e5f09
Niam5 <ebakkedahl@gmail.com>
2023-08-13 22:23:41 -0500

    Change out client patcher
    Finally found the necessary part of the X64 executable to patch allowing both 32 and 64 Bit clients work

---
+++ contrib/connection_patcher/Source/Client Patcher/Patterns/Windows.cs
@@ -0,0 +1,36 @@
+﻿/*
+ * Copyright (C) 2015  MaNGOS project <http://getmangos.eu>
+ * Copyright (C) 2012-2013 Arctium Emulation <http://arctium.org>
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 2 of the License, or (at your
+ * option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace CMaNGOSPatcher.Patterns
+{
+    class Windows
+    {
+        public static class x86
+        {
+            public static byte[] A1 = { 0xF3, 0xFF, 0xFF, 0xE8, 0xB1, 0xED, 0xFF, 0xFF, 0x85, 0xC0 };
+            public static byte[] A2 = { 0x83, 0xC4, 0x04, 0x84, 0xC0, 0x74, 0x23, 0x33, 0xC0, 0x8D };
+            public static byte[] A3 = { 0x8B, 0x55, 0x0C, 0x83, 0xFA, 0x02, 0x75, 0x73, 0x8D, 0x45 };
+        }
+
+        public static class x64
+        {
+            public static byte[] B1 = { 0xD2, 0xE8, 0xA8, 0x01, 0x74, 0x1A, 0x0F, 0x1F, 0x00, 0x48 };
+            public static byte[] B2 = { 0x41, 0x83, 0xFD, 0x02, 0x74, 0x08, 0x41, 0x8B, 0xD5, 0xE9 };
+        }
+    }
+}
---
+++ contrib/connection_patcher/readme.md
@@ -0,0 +1,12 @@
+== CMaNGOSPatcher ==
+
+This is a binary patching tool that will aid in making WoW 4.3.4 playable on
+unofficial servers, by patching out the server redirection (which involves
+the RSA algorithm).
+
+To run it: 
++ 1 - Compile source in release with Visual Studio C# or use the binary in the "Binary" folder.
++ 2 - Drag Wow.exe and Wow-64.exe over the patcher .exe icon, when prompted press enter.
++ 3 - You should now have Wow-patched.exe and Wow-64-Patched.exe in the WoW folder, these are your patched WoW files.
+
+Based on SkyFire and MaNGOS patcher code
