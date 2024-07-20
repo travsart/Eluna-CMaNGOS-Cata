1885b7ac9a2dda3a3314a38d1efc27442d717b16
Foereaper <foereaper@elunatech.com>
2024-07-20 17:32:03 +0200

    Update Eluna submodule

--- src/game/Maps/Map.cpp
+++ src/game/Maps/Map.cpp
@@ -131,7 +131,7 @@ Map::Map(uint32 id, time_t expiry, uint32 InstanceId, uint8 SpawnMode)
     // lua state begins uninitialized
     eluna = nullptr;
 
-    if (sElunaConfig->IsElunaEnabled() && !sElunaConfig->IsElunaCompatibilityMode() && sElunaLoader->ShouldMapLoadEluna(id))
+    if (sElunaConfig->IsElunaEnabled() && !sElunaConfig->IsElunaCompatibilityMode() && sElunaConfig->ShouldMapLoadEluna(id))
         eluna = new Eluna(this);
 
     if (Eluna* e = GetEluna())
